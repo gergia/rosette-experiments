04d7b4cbe7608d1a5852d190581b21f5531aa917
gavran <gavran@mpi-sws.org>
2018-04-17 16:54:06 +0200

    added approach using define synthax

--- combiningOperations.rkt
+++ combiningOperations.rkt
@@ -4,6 +4,7 @@
 
 ; the goal is to combine three different function, f, g and h, in a way to produce a desired result.
 ; h is identity and is there because I still don't know how to have variable number of steps (a symbolic value that a solver would decide about)
+; the third approach is by using define-synthax
 
 (define (f i)
   (+ i 2)
@@ -31,7 +32,7 @@
      (operation-list n)
      )
     )
-  
+
 
 
 
@@ -61,3 +62,21 @@
 ;)
 
 
+
+;
+; the way with define-synthax. As I am interested in particular values, I add this to the assumption of synthesize procedure
+;
+; surprisingly, leaving out the identity function (h) creates makes it unsat to create a combination of values less than bound.
+(define-synthax (prog-combination-power x d)
+  #:base x
+  #:else ([choose f g h] (prog-combination-power x (- d 1)))
+  )
+
+(define-symbolic a integer?)
+(print-forms
+ (synthesize
+  #:forall a
+  #:assume (assert (= a 10))
+  #:guarantee (assert (equal? (prog-combination-power a 10) ((lambda (t) 20) a)))
+  )
+ )
